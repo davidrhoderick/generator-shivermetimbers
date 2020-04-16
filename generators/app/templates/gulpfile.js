f347bde8c30aa6e925a8aa2decb1675ba7f2148a
David Rhoderick <david@artlyticalmedia.com>
2020-04-16 21:54:27 +0200

    Improving gulpfile as much as possible for now (sourcemaps not working)

--- generators/app/templates/gulpfile.js
+++ generators/app/templates/gulpfile.js
@@ -5,30 +5,36 @@ var gulp        = require('gulp'),
     jshint      = require('gulp-jshint'),
     minify      = require('gulp-minify'),
     sassLint    = require('gulp-sass-lint'),
-    notify      = require('gulp-notify');
+    notify      = require('gulp-notify'),
+    sourcemaps  = require('gulp-sourcemaps');
 
 gulp.task('sass', function(){
   return gulp.src('static/scss/style.scss')
-    .pipe(sass())
-      .on('error', notify)
+    .pipe(sourcemaps.init())
+    .pipe(sass({outputStyle: 'compressed'}))
+      .on('error', sass.logError)
     .pipe(sassLint({
       files: { ignore: 'static/bower_components/**/*.scss' }
     }))
+    .pipe(sourcemaps.write('./'))
     .pipe(gulp.dest('./'))
     .pipe(browserSync.stream());
 });
 
 gulp.task('js', function(){
   return gulp.src('static/js/site.js')
+    .pipe(sourcemaps.init()) // NOT WORKING, USE UNMINIFIED FILES FOR DEBUGGING WITH SOURCEMAPS
     .pipe(include())
       .on('error', notify)
     .pipe(jshint.reporter('default'))
     .pipe(minify({
+      noSource: true,
       ext:{
         src: '.js',
         min: '.min.js'
       }
     }))
+    .pipe(sourcemaps.write('./')) // NOT WORKING, USE UNMINIFIED FILES FOR DEBUGGING WITH SOURCEMAPS
     .pipe(gulp.dest('static'))
     .pipe(browserSync.stream());
 });
@@ -40,7 +46,7 @@ gulp.task('serve', ['sass', 'js'], function () {
 
   gulp.watch('static/scss/**/*.scss', ['sass']);
   gulp.watch('static/js/**/*.js', ['js']);
-  gulp.watch(['*.php', 'templates/**/*.twig']).on('change', browserSync.reload);
+  gulp.watch(['*.php', 'woocommerce/**/*.php', 'templates/**/*.twig', 'views/**/*.twig']).on('change', browserSync.reload);
 });
 
 gulp.task('default', ['serve']);
